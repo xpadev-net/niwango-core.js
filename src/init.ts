3c846ef5b915743cbb61a25f192ecc958dec6ab6
xpa <xpadev@gmail.com>
2023-05-30 03:00:14 +0900

    fix: export necessary utils

--- src/init.ts
+++ src/init.ts
@@ -1,19 +1,21 @@
 import { initConfig } from "@/config";
-import { execute, initDefinedFunctions, initPrototypeScope } from "@/context";
+import { initDefinedFunctions, initPrototypeScope } from "@/context";
 import { initExecute } from "@/executor";
 import { initResolvePrototype } from "@/prototype/resolve";
 import { initArgumentParser, initAssign, initGetName } from "@/utils";
 
 const initCore = () => {
   initResolvePrototype();
-  initPrototypeScope();
-  initDefinedFunctions();
   initGetName();
   initArgumentParser();
   initAssign();
   initConfig();
   initExecute();
-  console.log(execute);
 };
 
-export { initCore };
+const resetCore = () => {
+  initDefinedFunctions();
+  initPrototypeScope();
+};
+
+export { initCore, resetCore };
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,34 @@
 import {
   appendDefinedFunctions,
+  argumentParser,
+  assign,
   execute,
+  getName,
   prototypeScope,
+  resolvePrototype,
   setIsWide,
 } from "@/context";
-import { initCore } from "@/init";
+import * as Errors from "@/errors";
+import { initCore, resetCore } from "@/init";
 import { parseScript } from "@/parser/parse";
 
 initCore();
 
+const utils = {
+  argumentParser,
+  getName,
+  assign,
+  resolvePrototype,
+};
+
 class NiwangoCore {
   static execute = execute;
+  static utils = utils;
+  static resetCore = resetCore;
   static parseScript = parseScript;
   static appendDefinedFunctions = appendDefinedFunctions;
   static setIsWide = setIsWide;
+  static errors = Errors;
   static prototypeScope = prototypeScope;
   static default = NiwangoCore;
 }
