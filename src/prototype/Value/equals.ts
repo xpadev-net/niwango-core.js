04a74278a26d3a03ae28f1d9a3f9c7a67bd176c9
xpa <xpadev@gmail.com>
2023-06-01 21:59:27 +0900

    fix: typo

--- src/prototype/Value/equals.ts
+++ src/prototype/Value/equals.ts
@@ -1,9 +1,10 @@
 import { execute } from "@/context";
+import { Equality } from "@/operators";
 import { PrototypeValueFunction } from "@/prototype/Value/index";
 
 const processEquals: PrototypeValueFunction = (script, scopes, object) => {
   const value = execute(script.arguments[0], scopes);
-  return object === value;
+  return Equality(object, value);
 };
 
 export { processEquals };
