727788d45666edbbd86b7b630a6ecaf4109ee8e5
xpadev <xpadev@gmail.com>
2025-08-14 23:30:48 +0900

    fix: lefthook config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,13 @@
 import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import commonjs from '@rollup/plugin-commonjs';
-import {nodeResolve} from "@rollup/plugin-node-resolve";
-import json from '@rollup/plugin-json';
-import pkg from "./package.json" assert {type: 'json'};
-import * as path from "path";
-import {fileURLToPath} from "url";
+import pkg from "./package.json" assert { type: "json" };
+
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+
 const banner = `/*!
   niwango-core.js v${pkg.version}
   (c) 2023 xpadev-net https://xpadev.net
@@ -15,21 +17,21 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-    input: 'src/main.ts',
-    output: {
-        file: 'dist/niwango-core.js',
-        format: 'umd',
-        name: 'NiwangoCore',
-        banner
-    },
-    plugins: [
-        json(),
-        typescript(),
-        commonjs(),
-        babel({
-            babelHelpers: "bundled",
-            configFile: path.resolve(__dirname, ".babelrc.js"),
-        }),
-        nodeResolve(),
-    ]
-}
+  input: "src/main.ts",
+  output: {
+    file: "dist/niwango-core.js",
+    format: "umd",
+    name: "NiwangoCore",
+    banner,
+  },
+  plugins: [
+    json(),
+    typescript(),
+    commonjs(),
+    babel({
+      babelHelpers: "bundled",
+      configFile: path.resolve(__dirname, ".babelrc.js"),
+    }),
+    nodeResolve(),
+  ],
+};
