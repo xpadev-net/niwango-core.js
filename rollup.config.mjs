c8536d3fb78c9dd8ba9ecbd40e0d9fb7fa537347
xpadev <xpadev@gmail.com>
2025-08-14 23:31:34 +0900

    chore: support node v22/24

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import pkg from "./package.json" assert { type: "json" };
+import pkg from "./package.json" with { type: "json" };
 
 import * as path from "node:path";
 import { fileURLToPath } from "node:url";
