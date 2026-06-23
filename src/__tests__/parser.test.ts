45afb1d156f7acc90eb3007367f7b05ba17b75f2
xpadev <xpadev@gmail.com>
2026-06-24 06:56:16 +0900

    Fix parser syntax error boundary

---
+++ src/__tests__/parser.test.ts
@@ -0,0 +1,29 @@
+import { expect, test, vi } from "vitest";
+import { parseScript } from "@/parser/parse";
+import { SyntaxError as PeggySyntaxError, parse } from "@/parser/parser";
+
+test("parseScript surfaces syntax errors by default", () => {
+  const info = vi.spyOn(console, "info").mockImplementation(() => {});
+
+  try {
+    expect(() => parseScript("1@+2", "parser.test")).toThrow(PeggySyntaxError);
+    expect(info).not.toHaveBeenCalled();
+  } finally {
+    info.mockRestore();
+  }
+});
+
+test("parseScript character deletion recovery is opt-in", () => {
+  const info = vi.spyOn(console, "info").mockImplementation(() => {});
+
+  try {
+    expect(
+      parseScript("1@+2", "parser.test", { recoverSyntaxErrors: true }),
+    ).toEqual(parse("1+2", { grammarSource: "parser.test" }));
+    expect(() =>
+      parseScript("1+", "parser.test", { recoverSyntaxErrors: true }),
+    ).toThrow(PeggySyntaxError);
+  } finally {
+    info.mockRestore();
+  }
+});
